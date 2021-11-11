947becea833688e949fa801e12673a4b85880647
Luis Wirth <lwirth2000@gmail.com>
2021-11-11 16:53:34 +0100

    file select/save dialog

--- src/savefile.rs
+++ src/savefile.rs
@@ -1,6 +1,6 @@
 use crate::canvas::Curve;
 
-use std::{fs, path::Path};
+use std::{fs, path::PathBuf};
 
 use bevy::prelude::*;
 use bevy_egui::EguiContext;
@@ -12,19 +12,40 @@ impl Plugin for SavefilePlugin {
   }
 }
 
+const EXTENSION: &str = "co";
+const DEFAULT_FILE_NAME: &str = "carveout.co";
+fn default_dir_path() -> Option<PathBuf> {
+  dirs::cache_dir()
+}
+
 fn ui(egui: Res<EguiContext>, commands: Commands, curves: Query<(Entity, &Curve)>) {
   egui::Window::new("Savefile").show(egui.ctx(), |ui| {
-    let path = dirs::cache_dir().unwrap().join("canvas.co");
-
-    if ui.button("Load canvas").clicked() {
-      load_file(path, commands, curves);
-    } else if ui.button("Save canvas").clicked() {
-      save_file(path, curves);
+    if ui.button("📝").clicked() {
+      save_file(curves);
+    } else if ui.button("📂").clicked() {
+      load_file(commands, curves);
     }
   });
 }
 
-fn save_file(path: impl AsRef<Path>, curves: Query<(Entity, &Curve)>) {
+fn save_file(curves: Query<(Entity, &Curve)>) {
+  let mut dialog = rfd::FileDialog::new()
+    .set_title("Save carveout file")
+    .set_file_name(DEFAULT_FILE_NAME)
+    .add_filter("carveout", &["co"]);
+
+  match default_dir_path() {
+    Some(dir) => dialog = dialog.set_directory(dir),
+    None => {}
+  }
+
+  let mut path = match dialog.save_file() {
+    Some(p) => p,
+    None => return,
+  };
+
+  path.set_extension(EXTENSION);
+
   let curves: Vec<Curve> = curves.iter().map(|c| c.1).cloned().collect();
   let data = bincode::serialize(&*curves).unwrap();
   match fs::write(path, data) {
@@ -33,7 +54,21 @@ fn save_file(path: impl AsRef<Path>, curves: Query<(Entity, &Curve)>) {
   }
 }
 
-fn load_file(path: impl AsRef<Path>, mut commands: Commands, curves: Query<(Entity, &Curve)>) {
+fn load_file(mut commands: Commands, curves: Query<(Entity, &Curve)>) {
+  let mut dialog = rfd::FileDialog::new()
+    .set_title("Load carveout file")
+    .add_filter("carveout", &["co"]);
+
+  match default_dir_path() {
+    Some(dir) => dialog = dialog.set_directory(dir),
+    None => {}
+  }
+
+  let path = match dialog.pick_file() {
+    Some(p) => p,
+    None => return,
+  };
+
   match fs::read(path) {
     Ok(data) => {
       curves
