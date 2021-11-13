ee1204ee93bc499d63d92d56dccda1e86cda5456
Luis Wirth <lwirth2000@gmail.com>
2021-11-13 23:32:16 +0100

    add bevy runner throttle

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,25 @@ mod canvas;
 mod savefile;
 mod splines;
 mod toolbox;
+//mod render;
 
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 fn main() {
   App::build()
+    .insert_resource(bevy::app::ScheduleRunnerSettings::run_loop(std::time::Duration::from_millis(10)))
     .add_plugins(DefaultPlugins)
     .add_plugin(EguiPlugin)
-    //.add_plugin(splines::SplinesPlugin)
     .add_plugin(canvas::CanvasPlugin)
     .add_plugin(toolbox::ToolboxPlugin)
     .add_plugin(savefile::SavefilePlugin)
     .add_system(welcome_ui.system())
-    //.set_runner(throttle_system)
     .run();
 }
 
 fn welcome_ui(egui: Res<EguiContext>) {
   egui::Window::new("Welcome").show(egui.ctx(), |ui| {
-    ui.label("rust");
     ui.label("Welcome to Carveout.");
     ui.separator();
     ui.label("A tool for modern scientific digital pen note taking.");
